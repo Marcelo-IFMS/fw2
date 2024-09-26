fa54bf5c82f271fa32ca49919b30971d12a37d0c
Marcelo Tomporoski Perez <marcelotperez@gmail.com>
2024-09-26 08:16:46 -0400

    refact: navbar href

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -9,9 +9,9 @@ export default function NavBar() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link to="/">Home</Nav.Link>
-                    <Nav.Link to="/produtos">Produtos</Nav.Link>
-                    <Nav.Link to="/sobre">Sobre</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/produtos">Produtos</Nav.Link>
+                    <Nav.Link href="/sobre">Sobre</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">
