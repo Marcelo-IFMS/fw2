8880d848af469539af7ae3489f24cec03cdd1058
Marcelo Tomporoski Perez <marcelotperez@gmail.com>
2024-09-26 11:36:26 -0400

    create: Produtos/[ID].js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,9 @@ function Index() {
     <div>
       <NavBar />
       <Container className="conteudo">
-
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci non beatae aperiam laudantium, quod omnis ad delectus id excepturi quos eius temporibus voluptate distinctio mollitia porro! Animi dicta dolor eius.</p>
         <p>Nobis sapiente beatae eos aliquid facere cupiditate officiis, minus, voluptatem at odit ea dignissimos! Voluptatem, tenetur nemo non consectetur et obcaecati! Quasi pariatur iure facere dolores, enim aliquid temporibus impedit!</p>
         <p>Laudantium similique deleniti saepe praesentium id commodi voluptates quam porro mollitia non, sequi eius dolore vel libero quis optio rem necessitatibus cupiditate repudiandae. Neque doloremque animi porro nobis, at consectetur.</p>
-
       </Container>
       <Footer />
     </div>
--- src/pages/produtos/[id].js
+++ src/pages/produtos/[id].js
@@ -1,7 +1,16 @@
 import { useRouter } from "next/router"
+import { Container } from "react-bootstrap"
+import NavBar from "../components/navbar"
+import Footer from "../components/footer"
 
 export default function Page() {
     const router = useRouter()
-    return <p>Post: {router.query.id}</p>
-    
+    return <>
+        <NavBar />
+        <Container className="conteudo">
+            <p>Post: {router.query.id}</p>
+        </Container>
+        <Footer />
+    </>
+
 }
